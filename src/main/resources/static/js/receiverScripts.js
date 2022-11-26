d84c3d131041d0dc660620c501a78e14aeee179f
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-26 20:50:26 +0100

    rename folders, refactor

--- src/main/resources/static/js/receiverScripts.js
+++ src/main/resources/static/js/receiverScripts.js
@@ -48,7 +48,6 @@ function clearAllFieldsAction() {
     }
 }
 
-
 // CONTEXT CALLS
 registerClickAction(fillRandomBtnId, fillInputFieldsWithRandomNumbersAction);
 registerClickAction(clearBtnId, clearAllFieldsAction);
