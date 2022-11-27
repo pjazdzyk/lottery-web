836e3174e22a6cda9c979702546f71a4b229e1ec
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-27 15:12:04 +0100

    add scheduler for Redis cache clear after each draw date

---
+++ src/main/java/pl/lottery/infrastructure/schedulers/resultschecker/RedisClearingScheduler.java
@@ -0,0 +1,20 @@
+package pl.lottery.infrastructure.schedulers.resultschecker;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisClearingScheduler {
+    private final CacheManager cacheManager;
+
+    public RedisClearingScheduler(CacheManager cacheManager) {
+        this.cacheManager = cacheManager;
+    }
+
+    @Scheduled(cron = "${lotto.checker.lotteryRunOccurrence}")
+    void clearRedisCache() {
+        cacheManager.getCacheNames().forEach(cache -> cacheManager.getCache(cache).clear());
+    }
+
+}
