6cb463cab9b1e596321c65b31cf85bb6eb87b0f1
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-11-29 13:38:59 +0100

    change method name

--- src/main/java/pl/lottery/infrastructure/controllers/homepage/HomePageController.java
+++ src/main/java/pl/lottery/infrastructure/controllers/homepage/HomePageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomePageWebController {
+public class HomePageController {
 
     @GetMapping("/")
     public String showHomePageView(){
