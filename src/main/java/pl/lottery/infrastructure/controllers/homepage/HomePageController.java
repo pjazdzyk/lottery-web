290c741d399d5d16970859eda3b66dab99167d57
Piotr Jażdżyk <piotr.jazdzyk@o2.pl>
2022-12-01 10:33:31 +0100

    minor refactor

--- src/main/java/pl/lottery/infrastructure/controllers/homepage/HomePageController.java
+++ src/main/java/pl/lottery/infrastructure/controllers/homepage/HomePageController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
-
     @GetMapping("/")
     public String showHomePageView(){
         return "index";
--- src/main/java/pl/lottery/infrastructure/schedulers/resultschecker/RedisClearingScheduler.java
+++ src/main/java/pl/lottery/infrastructure/schedulers/resultschecker/RedisClearingScheduler.java
@@ -5,7 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RedisClearingScheduler {
+class RedisClearingScheduler {
     private final CacheManager cacheManager;
 
     public RedisClearingScheduler(CacheManager cacheManager) {
