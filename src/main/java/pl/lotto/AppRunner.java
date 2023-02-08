009662cf5f9c0d292374b9ff02f8be89a8ea201f
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-02-08 16:06:54 +0100

    swagger

--- src/main/java/pl/lotto/AppRunner.java
+++ src/main/java/pl/lotto/AppRunner.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 //@EnableCaching
 @EnableScheduling
+@EnableWebMvc
 public class AppRunner {
 
     public static void main(String[] args) {
--- src/main/java/pl/lotto/infrastructure/resultsannouncer/controllers/ResultsAnnouncerWebController.java
+++ src/main/java/pl/lotto/infrastructure/resultsannouncer/controllers/ResultsAnnouncerWebController.java
@@ -1,28 +1,28 @@
-package pl.lotto.infrastructure.resultsannouncer.controllers;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import pl.lotto.resultsannouncer.ResultsAnnouncerFacade;
-import pl.lotto.resultsannouncer.dto.AnnouncerRequestDto;
-import pl.lotto.resultsannouncer.dto.AnnouncerResponseDto;
-
-import java.util.UUID;
-
-@Controller
-public class ResultsAnnouncerWebController {
-
-    private final ResultsAnnouncerFacade resultsAnnouncerFacade;
-
-    public ResultsAnnouncerWebController(ResultsAnnouncerFacade resultsAnnouncerFacade) {
-        this.resultsAnnouncerFacade = resultsAnnouncerFacade;
-    }
-
-    @GetMapping("/results")
-    public String getResultsForUuid(AnnouncerRequestDto announcerRequestDto, Model model) {
-        AnnouncerResponseDto announcerResponseDto = resultsAnnouncerFacade.getResultsForId(announcerRequestDto.getRequestUuid());
-        model.addAttribute("announcerResponseDto", announcerResponseDto);
-        return "announcer-resp-view";
-    }
-
-}
+//package pl.lotto.infrastructure.resultsannouncer.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.*;
+//import pl.lotto.resultsannouncer.ResultsAnnouncerFacade;
+//import pl.lotto.resultsannouncer.dto.AnnouncerRequestDto;
+//import pl.lotto.resultsannouncer.dto.AnnouncerResponseDto;
+//
+//import java.util.UUID;
+//
+//@Controller
+//public class ResultsAnnouncerWebController {
+//
+//    private final ResultsAnnouncerFacade resultsAnnouncerFacade;
+//
+//    public ResultsAnnouncerWebController(ResultsAnnouncerFacade resultsAnnouncerFacade) {
+//        this.resultsAnnouncerFacade = resultsAnnouncerFacade;
+//    }
+//
+//    @GetMapping("/results")
+//    public String getResultsForUuid(AnnouncerRequestDto announcerRequestDto, Model model) {
+//        AnnouncerResponseDto announcerResponseDto = resultsAnnouncerFacade.getResultsForId(announcerRequestDto.getRequestUuid());
+//        model.addAttribute("announcerResponseDto", announcerResponseDto);
+//        return "announcer-resp-view";
+//    }
+//
+//}
